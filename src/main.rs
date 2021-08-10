db5e658fb0e7f671c6c48ee64ded1a0ded583ef3
paperbenni <paperbenni@gmail.com>
2021-08-10 23:15:40 +0200

    init rendering an applet script

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,19 @@
 use dirs::config_dir;
 use std::path::{Path, PathBuf};
+use std::process::Command;
+use std::str;
+
+pub struct Statusdata {
+    configpath: String,
+}
+
+impl Statusdata {
+    fn new() -> Statusdata {
+        Statusdata {
+            configpath: String::from(config_dir().unwrap().to_str().unwrap()),
+        }
+    }
+}
 
 enum Appletlocation {
     User,
@@ -13,15 +27,51 @@ pub struct Applet {
 }
 
 impl Applet {
-    fn render(&self) {
-        print!("^f{}^", 11);
-        print!("{}", self.script);
+    // fn getpath(&self) -> String {
+    //     match self.location {
+    //         User => {
+    //             return String::from(&format("{}"))
+    //         }
+    //     }
+    //     String::from("asd")
+    // }
+
+    fn render(&self, data: &Statusdata) -> Option<String> {
+
+        let scriptpath = match &self.location {
+            User => format!(
+                "{}/instantstatus/applets/{}.ist.sh",
+                &data.configpath, &self.script
+            ),
+            Global => format!(
+                "/usr/share/instantstatus/applets/{}.ist.sh",
+                &self.script
+            ),
+        };
+
+        match Command::new("bash")
+            .arg("-c")
+            .arg(&format!("cd && source {} && status_display", &scriptpath))
+            .output()
+        {
+            Ok(output) => {
+                return Some(String::from(str::from_utf8(&output.stdout).unwrap()));
+            }
+            Err(_) => {}
+        };
+        None
     }
 
     fn new(name: String) -> Option<Applet> {
         let mut appletpath = config_dir().unwrap();
-        appletpath.push(Path::new(&format!("applets/{}.ist.sh", &name)));
-        
+
+        appletpath.push(Path::new(&format!(
+            "instantstatus/applets/{}.ist.sh",
+            &name
+        )));
+
+        println!("{}", appletpath.to_str().unwrap());
+
         let mut location = Appletlocation::Global;
 
         if appletpath.is_file() {
@@ -47,12 +97,15 @@ impl Applet {
 }
 
 fn main() {
+    let data = Statusdata::new();
     let mut confdir = config_dir().unwrap();
-    confdir.push(Path::new("instantstatus"));
+    confdir.push(Path::new("instantstatus/applets"));
 
     if !confdir.exists() {
         std::fs::create_dir_all(confdir).unwrap();
     }
 
-    Applet::new(String::from("hello")).unwrap().render();
+    let tester2 = Applet::new(String::from("hello")).unwrap();
+    let tester = tester2.render(&data).unwrap();
+    println!("{}", tester)
 }
